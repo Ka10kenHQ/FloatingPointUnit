cc53121f18219a74a80fcdae9f12bba91454d9ad
mate <matekopaliani12@gmail.com>
2025-02-23 20:06:33 +0400

    adding my work.1

--- components/multiplier/sigfmd.sv
+++ components/multiplier/sigfmd.sv
@@ -69,7 +69,6 @@ always @(oe1[0] & oe1[1] & ~oe2) begin
                 $display("im here 1");
                 fa_in = {2'b01, look_up[7:0], 48'b0};
                 fb_in = {fb , 5'b0};
-                iter = iter + 1;
             end
             else if(iter == 2) begin
                 $display("im here 2");
@@ -79,35 +78,44 @@ always @(oe1[0] & oe1[1] & ~oe2) begin
                 oe2_reg = 1'b1;
             end
             Dcnt = Dcnt - 1;
+            $display("Dcnt = %d", Dcnt);
         end
         else if(~oe1_reg[0] & oe1_reg[1] & oe2_reg) begin
             fa_in = ~mul_out[115:90];
             fb_in = mul_out[115:68];
         end
+        $display("iter = %d", iter);
         iter = iter + 1;
 
-        if(iter == 5 && Dcnt > 0) iter = 1;
-        else if(iter == 5 && Dcnt == 0) begin
+        if(iter == 5 && Dcnt > 0) begin 
+            iter = 1;
+        end
+        else if(Dcnt == 0) begin
             oe1_reg = 2'b01;
             oe2_reg = 1'b1;
         end
     end while(Dcnt > 0);
+
     $display("then i am here");
     for(i = 1; i < 5; i = i +1) begin
         if(~oe1_reg[1] & oe1_reg[0] & oe2_reg) begin
             fa_in = {fa, 5'b0};
             fb_in = mul_out[115:68];
             oe2_reg = 1'b0;
+            $display("Am i inside?");
         end
         else if(~oe1_reg[1] & oe1_reg[0] & ~oe2_reg) begin
             fa_in = {fa, 5'b0};
             fb_in = {mul_out[115:68]};
             oe1_reg = 2'b00;
             oe2_reg = 1'b0;
+            $display("Am i inside 2 ?");
         end
         else if(~oe1_reg[1] & ~oe1_reg[0] & ~oe2_reg) begin
             fa_in = {mul_out[115:90], ({29{db}} & mul_out[89:61]), 3'b0};
             fb_in = {fb, 5'b0};
+            $display("Am i inside 3 ?");
+
         end
     end
 end
